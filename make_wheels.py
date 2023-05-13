fdb570f019b1d4f0ca15f0e1e6669a401e5fca5d
Catherine <whitequark@whitequark.org>
2023-05-13 05:09:37 +0000

    Use os.execv to run Zig on POSIX platforms.
    Fixes #6.

--- make_wheels.py
+++ make_wheels.py
@@ -94,11 +94,12 @@ def write_ziglang_wheel(out_dir, *, version, platform, archive):
 
             if entry_name.startswith('zig'):
                 contents['ziglang/__main__.py'] = f'''\
-import os, sys, subprocess
-sys.exit(subprocess.call([
-    os.path.join(os.path.dirname(__file__), "{entry_name}"),
-    *sys.argv[1:]
-]))
+import os, sys
+argv = [os.path.join(os.path.dirname(__file__), "{entry_name}"), *sys.argv[1:]]
+if os.name == 'posix':
+    os.execv(argv[0], argv)
+else:
+    import subprocess; sys.exit(subprocess.call(argv))
 '''.encode('ascii')
 
     with open('README.pypi.md') as f:
